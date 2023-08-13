4d3b3bbfef154026713ef4ad18b35b0a617f8318
BurgerRelish <23640103@sun.ac.za>
2023-08-13 15:50:01 +0200

    Reorder files

--- src/brotli/include/brotli/decode.h
+++ src/brotli/include/brotli/decode.h
@@ -12,9 +12,9 @@
 #ifndef BROTLI_DEC_DECODE_H_
 #define BROTLI_DEC_DECODE_H_
 
-#include <brotli/port.h>
-#include <brotli/shared_dictionary.h>
-#include <brotli/types.h>
+#include <brotli/include/brotli/port.h>
+#include <brotli/include/brotli/shared_dictionary.h>
+#include <brotli/include/brotli/types.h>
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
--- src/brotli/include/brotli/encode.h
+++ src/brotli/include/brotli/encode.h
@@ -12,9 +12,9 @@
 #ifndef BROTLI_ENC_ENCODE_H_
 #define BROTLI_ENC_ENCODE_H_
 
-#include <brotli/port.h>
-#include <brotli/shared_dictionary.h>
-#include <brotli/types.h>
+#include <brotli/include/brotli/port.h>
+#include <brotli/include/brotli/shared_dictionary.h>
+#include <brotli/include/brotli/types.h>
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
